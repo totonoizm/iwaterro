2ef18eba51da9cb1e2c27a1ddfc7ac363337c67d
Takeshi Ota <singer.scottie@gmail.com>
2020-11-14 17:02:39 +0900

    コメントモデル作成、アソシエーション設定

--- app/models/post_gym.rb
+++ app/models/post_gym.rb
@@ -2,4 +2,5 @@ class PostGym < ApplicationRecord
 
   belongs_to :user
   attachment :image
+  has_many :post_comments, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_055255) do
+ActiveRecord::Schema.define(version: 2020_11_14_075933) do
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "post_gyms", force: :cascade do |t|
     t.text "gym_name"
